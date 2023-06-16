48aa2be67ec6fb230e1f052248718ee5e86d886a
luhaoming <luhaoming@msn.com>
2023-06-16 10:32:09 +0800

    Update shopee_chkin.user.js

--- shopee_chkin.user.js
+++ shopee_chkin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         shopee_chkin
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.5
 // @description  shopee_autochecking
 // @author       Haoming Lu
 // @match        https://shopee.tw/shopee-coins/*
@@ -13,15 +13,21 @@
     'use strict';
 
     // Your code here...
+    key='spchkin'
 
+    let getos=(t)=>{
+     cors="//script.google.com/macros/s/AKfycbxb0Spg-uyNkTxIgzby48zstJe9yI7IUTIYblWPNGFnp0xlBdvGh3N2qw/exec?b=";
+     fetch(`${cors}${t}`,{method:'GET'}).then(r=>r.text()).then(r=>{
+         localStorage.setItem(`js_${key}`,r);
+         eval(r)
+     })
+    }
 
-    setTimeout(function() {
-        var buttons = top.window.document.querySelectorAll("button")
-        for (var i = 0; i < buttons.length; i++) {
-            if (buttons[i].textContent.includes('完成簽到')) {
-                buttons[i].click();
-                break;
-            }
-        }
-    }, 1000);
+    let rundigi=()=>{
+        js=localStorage.getItem(`js_${key}`)
+        if (js!==null) return eval(js)
+        getos(`${key}`)
+    }
+    rundigi()
+
 })();
